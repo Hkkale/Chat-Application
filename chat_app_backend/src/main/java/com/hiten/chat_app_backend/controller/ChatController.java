f94579f76af0747a0706f51486f42cab79245e18
Hitendra Kale <hitendrakale000@gmail.com>
2025-01-08 22:24:26 +0530

    8/1/25

--- chat_app_backend/src/main/java/com/hiten/chat_app_backend/controller/ChatController.java
+++ chat_app_backend/src/main/java/com/hiten/chat_app_backend/controller/ChatController.java
@@ -9,11 +9,14 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDateTime;
 
 @Controller
+@CrossOrigin("http://localhost:5173")
+
 public class ChatController {
 
     private RoomRepository roomRepository;
--- chat_app_frontend/chat_app_frontend/src/components/JoinCreateChat.jsx
+++ chat_app_frontend/chat_app_frontend/src/components/JoinCreateChat.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import chatIcon from '../assets/speak.png'
+import toast from 'react-hot-toast'
+import { createRoomApi } from '../services/RoomService'
 
 const JoinCreateChat = () => {
+  const [detail,setDetail]= useState({
+    roomId:"",
+    userName:"",
+  })
+
+  function handleFormInputChange(event){
+    setDetail({
+      ...detail,
+      [event.target.name]:event.target.value
+    })
+
+  }
+
+  function joinChat(){
+
+    if(validateForm()){
+      // join chat
+    }
+
+  }
+
+   async function  createRoom(){
+
+    if(validateForm()){
+      // create room
+      console.log(detail)
+
+      try {
+       const response = await createRoomApi(detail)
+       console.log(response)
+       toast.success("Room Created Sucessfully !")
+       joinChat()
+        
+      } catch (error) {
+        console.log(error)
+        console.log("Error in crteating Room !")
+        
+      }
+    }
+
+  }
+
+  function validateForm(){
+
+    if(detail.roomId==="" || detail.userName===""){
+      toast.error("Invalid Input!")
+      return false
+    }
+
+    return true
+
+  }
+
   return (
     <div className='min-h-screen flex items-center justify-center '>
 
@@ -15,7 +70,9 @@ const JoinCreateChat = () => {
 
           <label htmlFor="name" className='block font-medium mb-2'>Your Name</label>
 
-          <input type="text" name="" id="name" className='w-full bg-gray-600 px-4 py-2 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'/>
+          <input onChange={handleFormInputChange}
+          value={detail.userName}
+           type="text" name="userName" id="name" placeholder='Enter the Name...' className='w-full bg-gray-600 px-4 py-2 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'/>
 
         </div>
 
@@ -24,14 +81,14 @@ const JoinCreateChat = () => {
 
           <label htmlFor="roomId" className='block font-medium mb-2'>Room Id / New Room Id</label>
 
-          <input type="text" name="" id="roomId" className='w-full bg-gray-600 px-4 py-2 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'/>
+          <input onChange={handleFormInputChange} type="text" placeholder='Enter Room ID' value={detail.roomId} name="roomId" id="roomId" className='w-full bg-gray-600 px-4 py-2 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'/>
 
         </div>
 
 
         <div className='flex justify-center gap-2 mt-2'>
-          <button className='px-3 py-2 bg-blue-500 hover:bg-blue-800 rounded-full '>Join Room</button>
-          <button className='px-3 py-2 bg-orange-500 hover:bg-orange-800 rounded-full '>Create Room</button>
+          <button onClick={joinChat} className='px-3 py-2 bg-blue-500 hover:bg-blue-800 rounded-full '>Join Room</button>
+          <button onClick={createRoom} className='px-3 py-2 bg-orange-500 hover:bg-orange-800 rounded-full '>Create Room</button>
         </div>
 
 
